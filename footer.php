48479807ebe2e85905c005e68d1b0c523d16eea4
Megumi fujino <>
2020-12-09 04:44:27 +0900

    header foter追加

---
+++ footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer_line">
+        Copyright &copy; Shinohara.　ALL RIGHTS RESERVED.
+    </div>
+</footer>
+<!--------------------- ここまで footer---------------------->
+
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+    <script defer src="<?php echo get_template_directory_uri(); ?>/js/american_village.js"></script>
+  </body>
+</html>
