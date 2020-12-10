248dcca8b91cf225f55851e27f0e8b4143cc2446
Megumi fujino <>
2020-12-10 13:55:57 +0900

    レイアウト変更

--- header.php
+++ header.php
@@ -1,4 +1,5 @@
-<html lang="ja"><head>
+<html lang="ja">
+    <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
