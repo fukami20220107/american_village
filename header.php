98b221203e63b00ce295a54d155ede24955a4574
Kitano Yudai <>
2020-12-10 14:14:12 +0900

    aaa

--- header.php
+++ header.php
@@ -1,4 +1,6 @@
-<html lang="ja"><head>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
